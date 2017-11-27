99f803743a267e345f5a55c092b320eb35ef1cce
Andres Bonilla <abonilla.esc@gmail.com>
2017-11-27 13:22:01 +0100

    Create shoot method

--- Space Invaders/GameScene.swift
+++ Space Invaders/GameScene.swift
@@ -11,6 +11,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let player = SKSpriteNode(imageNamed: "player")
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -18,10 +20,26 @@ class GameScene: SKScene {
         background.zPosition = 0
         self.addChild(background)
         
-        let player = SKSpriteNode(imageNamed: "player")
         player.setScale(1)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         player.zPosition = 2
         self.addChild(player)
+    }
+    
+    func shoot() {
+        let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        shoot()
     }
 }
