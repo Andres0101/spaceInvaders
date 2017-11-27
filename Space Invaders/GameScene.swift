fcf8d7bbb5fa41c3fbaa9001c08266b439c448e6
Andres Bonilla <abonilla.esc@gmail.com>
2017-11-27 15:56:38 +0100

    Create boundaries in gameArea

--- Space Invaders/GameScene.swift
+++ Space Invaders/GameScene.swift
@@ -13,6 +13,20 @@ class GameScene: SKScene {
     
     let player = SKSpriteNode(imageNamed: "player")
     let shootSound = SKAction.playSoundFileNamed("shoot.mp3", waitForCompletion: false)
+    var gameArea: CGRect
+    
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let areaWidth = size.height / maxAspectRatio
+        let margin = (size.width - areaWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: areaWidth, height: size.height)
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
@@ -53,6 +67,14 @@ class GameScene: SKScene {
             let amountDragged = currentTouch.x - previousTouch.x
             
             player.position.x += amountDragged
+            
+            //Création des limites pour que le joueur ne quitte pas le gameArea
+            if  player.position.x > (gameArea.maxX - player.size.width/2) {
+                player.position.x = gameArea.maxX - player.size.width/2
+            }
+            if  player.position.x < (gameArea.minX + player.size.width/2) {
+                player.position.x = gameArea.minX + player.size.width/2
+            }
         }
     }
 }
