78c674f0dc4587a469160003e2e22ca6f0cf3cc1
Andres Bonilla <abonilla.esc@gmail.com>
2018-01-11 16:12:11 +0100

    Enable googleSignin action

--- Space Invaders/SignInViewController.swift
+++ Space Invaders/SignInViewController.swift
@@ -12,14 +12,25 @@ import GoogleSignIn
 
 class SignInViewController: UIViewController, GIDSignInUIDelegate {
 
+    @IBOutlet var space: UILabel!
+    @IBOutlet var invaders: UILabel!
+    @IBOutlet weak var googleButton: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         GIDSignIn.sharedInstance().uiDelegate = self
+        
+        googleButton.style = GIDSignInButtonStyle.wide
+        
+        //Ajouter propriété Kern aux labels
+        space.attributedText = NSAttributedString(string: "Space",attributes:[ NSAttributedStringKey.kern: 5])
+        invaders.attributedText = NSAttributedString(string: "Invaders",attributes:[ NSAttributedStringKey.kern: 5])
+    }
+    @IBAction func googleSignin(_ sender: Any) {
         GIDSignIn.sharedInstance().signIn()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
